006430feb2452cb90864dbb1b0921270655bafcd
Marcos Rufino <marcosrufino@Rufino-MacBook-Pro.local>
2020-03-03 20:39:31 -0300

    atribuir valores para classe Desafio02

--- src/main/java/com/desafio2/desafio02/Desafio02Application.java
+++ src/main/java/com/desafio2/desafio02/Desafio02Application.java
@@ -1,13 +1,26 @@
 package com.desafio2.desafio02;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Desafio02Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Desafio02Application.class, args);
+
+	}
+
+
+	static class Desafio02{
+
+		private String valor01 = "Ibyte";
+
+		private String valor02 = "Goldentec";
+
 	}
 
 }
